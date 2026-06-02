49ee1af147ad4da718c96829e2b5ac5c4c0a587d
limon313 <cj06338@gmail.com>
2026-06-02 22:06:17 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgresql://poer_db_user:7rcZVTdlclrCcMhsygkcFo9ECgcqUuAr@dpg-d8c0f34ua318fk3j5f50-a/poer_db',
-  ssl: { rejectUnauthorized: false }
+  connectionString: postgresql://poer_db_user:7rcZVTdlclrCcMpTWPUMLc9ECgcqUuAr@dpg-d8c0f34ua31s739j5f50-a.oregon-postgres.render.com/poer_db
 });
 
 module.exports = pool;
