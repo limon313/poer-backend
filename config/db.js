daa19dd8ab6e5652fb08255529a43694e34b0fe1
limon313 <cj06338@gmail.com>
2026-06-30 02:31:49 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,10 @@
 const { Pool } = require('pg');
-
 const pool = new Pool({
-  connectionString: 'postgresql://poer_db_user:7rcZVTdlclrCcMpTWPUMLc9ECgcqUuAr@dpg-d8c0f34ua31s739j5f50-a.oregon-postgres.render.com/poer_db',
+  connectionString: 'postgresql://poer_db_iht0_user:dY9EvQshZ1ryGziSnF32T1udXGegIRfW@dpg-d91furgk1i2s73asa4kg-a.oregon-postgres.render.com/poer_db_iht0',
   ssl: { rejectUnauthorized: false },
   max: 5,
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 10000,
-  keepAlive: true
+  connectionTimeoutMillis: 10000
 });
-
-pool.on('error', (err) => {
-  console.error('Beklenmeyen pool hatasi:', err.message);
-});
-
+pool.on('error', (err) => console.error('Pool hatasi:', err.message));
 module.exports = pool;
